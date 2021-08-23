dd7a56e3a18f372f55303e027337b38f3838cc35
taichaaan <54344033+taichaaan@users.noreply.github.com>
2021-08-23 13:34:41 +0900

    Update gulpfile.js

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -488,7 +488,7 @@ const watchTask = () => {
 		+ "\n"
 		+ "\n" + '   @name    : gulp watch'
 		+ "\n" + '   @task    : pug,ejs,sass,js,img,sprite,move'
-		+ "\n" + '   @version : 2.7.2'
+		+ "\n" + '   @version : 2.7.3'
 		+ "\n" + '   @gulp    : 4.0.2'
 		+ "\n" + '   @node    : 14.14.0'
 		+ "\n"
